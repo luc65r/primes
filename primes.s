e905a9f613dbe9e4976133d15ecc693f7af64556
luc65r <lucas@ransan.tk>
2020-08-22 17:34:32 +0200

    Documentation and license

--- primes.s
+++ primes.s
@@ -1,5 +1,37 @@
-.set BUF_LEN, 2000000 # 2 MB
+/*
+ * Copyright (C) 2020 Lucas Ransan <lucas@ransan.tk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+/* This program uses the sieve of Eratosthenes
+   (https://wikipedia.org/wiki/Sieve_of_Eratosthenes)
+   to print all the primes up to and including the argument num.
+
+   We use a bit array to represent all the numbers from 0 to num.
+   The first bit represents 0, the second 1, the third 2, ...
+   A set bit represents a prime number, so after eliminating all
+   the non primes, the bit array should look like that:
+     001101010001010001010...
+*/
+
+
+/* Length of the buffer for writing to stdout */
+.set BUF_LEN, 2048000 # 2 MB
+
+
+/* Initialized memory */
 	.data
 arg_err:
 	.ascii "Please supply one argument\n"
@@ -10,143 +42,178 @@ num_err:
 	.set ne_len, . - num_err
 
 
-	#.bss
+/* Uninitialized memory */
+	.bss
+
+/* 64 bit unsigned integer we print primes up to */
 num:
 	.quad 0
+
+/* Square root of num */
 sqrt_num:
 	.quad 0
 
+/* Address to the sys_mmap'ed bit array */
 primes:
 	.quad 0
 
+/* The buffer for writing to stdout */
 char_buf:
 	.fill BUF_LEN
+
 
 
 	.globl _start
 	.text
 _start:
-	# Read argc
+	/* Read argc and jump if it is 2 (program name + number)
+	   (%rsp) = argc
+	   8(%rsp) = argv[0]
+	   16(%rsp) = argv[1]
+	*/
 	movq	(%rsp), %rbx
 	cmpq	$2, %rbx
 	je	.argc_good
 
-	# Print error
+	/* Write error to stderr */
 	movq	$1, %rax # sys_write
-	movq	$2, %rdi # fd 2
-	movq	$arg_err, %rsi
-	movq	$ae_len, %rdx
+	movq	$2, %rdi # fd
+	movq	$arg_err, %rsi # buf
+	movq	$ae_len, %rdx # count
 	syscall
 
-
-	# Return with code 1
-	movq	$60, %rax
-	movq	$1, %rdi
+	/* Exit with code 1 */
+	movq	$60, %rax # sys_exit
+	movq	$1, %rdi # error_code
 	syscall
 
 
 .argc_good:
-	# Get the number
-	movq	16(%rsp), %rdi
-	xorq	%rsi, %rsi
-	movq	$10, %rdx
+	/* Read the number for argv[1] with strtoumax */
+	movq	16(%rsp), %rdi # str = argv[1]
+	xorq	%rsi, %rsi # endptr = null
+	movq	$10, %rdx # base
 	call	strtoumax
+	/* Store the result in num and jump if != 0 */
 	movq	%rax, num
-
 	testq	%rax, %rax
 	jnz	.number_good
 	
-	# Print error
+	/* Write error to stderr */
 	movq	$1, %rax # sys_write
-	movq	$2, %rdi # fd 2
-	movq	$num_err, %rsi
-	movq	$ne_len, %rdx
+	movq	$2, %rdi # fd
+	movq	$num_err, %rsi # buf
+	movq	$ne_len, %rdx # count
 	syscall
 
-	# Return with code 1
-	movq	$60, %rax
-	movq	$1, %rdi
+	/* Exit with code 1 */
+	movq	$60, %rax # sys_exit
+	movq	$1, %rdi # error_code
 	syscall
+
 
 .number_good:
+	/* If num == 1, we can jump to the end,
+	   because we know 1 isn't prime.
+	*/
 	cmpq	$1, num
 	je	.finish
 
-	# Allocate n / 8 + 1 bytes of memory
+	/* Allocate memory for num + 1 (0, 1, 2, ..., num) bits,
+	   which is num / 8 + 1 bytes.
+	*/
 	movq	$9, %rax # sys_mmap
-	xorq	%rdi, %rdi # addr = 0
+	xorq	%rdi, %rdi # addr = null
 	movq	num, %rsi
 	shrq	$3, %rsi
 	incq	%rsi # len = n / 8 + 1
 	movq	$3, %rdx # prot = read | write
 	movq	$0x22, %r10 # flags = private | anonymous
-	movq	$-1, %r8 # fd = -1
-	xorq	%r9, %r9 # offset = 0
+	movq	$-1, %r8 # fd
+	xorq	%r9, %r9 # offset
 	syscall
+	/* Store the address of the allocated memory in primes and %r15 */
 	movq	%rax, primes
 	movq	%rax, %r15
 
+	/* Set 2, 3, 5, 7, and all odds as potential primes */
 	movq	primes, %rdi
 	movb	$0b00110101, (%rdi)
-	incq	%rdi
-	movl	$0b01010101, %esi
+	incq	%rdi # ptr = primes[1]
+	movl	$0b01010101, %esi # value
 	movq	num, %rdx
-	shrq	$3, %rdx
+	shrq	$3, %rdx # num = num / 8
 	call	memset
 
+	/* Calculate the square root of num with the FPU (rounded down) */
 	push	num
 	fildq	(%rsp)
 	fsqrt
 	fistpq	(%rsp)
 	pop	sqrt_num
-	# sqrt_num = sqrt(num)
 
-	# Eliminate all non primes
+
+	/* We eliminate all non primes by setting
+	   the correponding bit to 0 in the bit array.
+
+	   %rbx: counter from 3 to sqrt_num incremented by 2
+	*/
 	movq	$1, %rbx
 .elim_loop:
 	addq	$2, %rbx
 	cmpq	sqrt_num, %rbx
 	ja	.end_elim
 
+	/* If the bit corresponding to the number in the counter
+	     primes[%rbx / 8] >> (7 - %rbx % 8)
+	   isn't set, we jumpt to the start of the loop (.elim_loop).
+	*/
 	movq	%rbx, %rax
-	shrq	$3, %rax
-	movb	(%r15, %rax), %al
+	shrq	$3, %rax # %rax = %rbx / 8
+	movb	(%r15, %rax), %al # %al = primes[%rax]
 
 	movq	%rbx, %rdx
-	andb	$0b111, %dl
+	andb	$0b111, %dl # %dl = %rbx % 8
 	movb	$7, %cl
-	subb	%dl, %cl
-	# %cl = 7 - %rdx % 8
+	subb	%dl, %cl # %cl = 7 - %dl
 
-	shrb	%cl, %al
+	shrb	%cl, %al # %al >>= %cl
 	andb	$1, %al
 	jz	.elim_loop
 
+
+	/* The bit corresponding to %rbx is set, so we eliminate
+	   (set to 0) all multiples of %rbx.
+	     primes[%r8 * %rbx / 8] &= ~(1 << (7 - (%r8 * %rbx) % 8))
+	   We only need to eliminate odds multiples of %rbx,
+	   because we didn't set the evens to 1 in the first place.
+	     odd * odd = odd
+	     _   * _   = even
+
+	   %r8: counter from 3 to num incremented by 2
+	*/
 	movq	$1, %r8
 .inner_loop:
 	addq	$2, %r8
 	movq	%r8, %rax
-	mulq	%rbx
+	mulq	%rbx # %rax = %r8 * %rbx
 	cmpq	num, %rax
 	ja	.end_inner
 
 	movq	%rax, %rdx
-	shrq	$3, %rdx
-	movb	(%r15, %rdx), %r10b
+	shrq	$3, %rdx # %rdx = %rax / 8
+	movb	(%r15, %rdx), %r10b # %r10b = primes[%rdx]
 
-	movq	%rax, %r9
-	andb	$0b111, %r9b
+	andb	$0b111, %al # %al = %rax % 8
 	movb	$7, %cl
-	subb	%r9b, %cl
-	# %cl = 7 - %rax % 8
+	subb	%al, %cl # %cl = 7 - %al
 
 	movb	$1, %r11b
 	shlb	%cl, %r11b
-	notb	%r11b
-	# %r11b = ~(1 << (7 - %rax % 8))
+	notb	%r11b # %r11b = ~(1 << %cl)
 
 	andb	%r11b, %r10b
-	movb	%r10b, (%r15, %rdx)
+	movb	%r10b, (%r15, %rdx) # primes[%rdx] = %r10b & %r11b
 
 	jmp	.inner_loop
 .end_inner:
@@ -154,11 +221,14 @@ _start:
 	jmp	.elim_loop
 .end_elim:
 
-	# Print '2'
-	movb	$0x32, %dil
+	/* Now that all the bits corresponding to primes are set to 1,
+	   and the others are set to 0, we can start to print!
+
+	   Print "2\n" here, because we don't print even numbers in the loop.
+	*/
+	movb	$0x32, %dil # '2'
 	call	putchar
-	# Print '\n'
-	movb	$0xA, %dil
+	movb	$0xA, %dil # '\n'
 	call	putchar
 
 	# Print the numbers
@@ -219,13 +289,13 @@ _start:
 
 	jmp	.print_loop
 .end_print:
-
-
-.finish:
 	movq	stdout, %rdi
 	call	fflush
 
-	# Return with code 0
-	movq	$60, %rax
-	xorq	%rdi, %rdi
+.finish:
+	/* Exit with code 0 */
+	movq	$60, %rax # sys_exit
+	xorq	%rdi, %rdi # error_code
 	syscall
+
+# vim: et! ts=8 sts=8 sw=8
